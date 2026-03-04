6792ba8991ffbc1c0bcc44fe012dfd92928c43b8
jweber <jweber@student.42Lyon.fr>
2026-03-04 16:14:19 +0100

    added files for ex02, must still put everything in a .tpp file

---
+++ ex02/Array.cpp
@@ -0,0 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jweber <jweber@student.42Lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 12:57:45 by jweber            #+#    #+#             */
+/*   Updated: 2026/03/04 13:56:46 by jweber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_TPP
+# define ARRAY_TPP
+
+
+#include <string>
+template<typename T>
+class Array
+{
+	public:
+		Array<T>();
+		Array<T>(unsigned int n);
+		Array<T>(const Array<T>& other);
+		~Array<T>();
+
+		Array<T>&	operator=(const Array<T>& other);
+
+		unsigned int	size();
+		const T&	operator[](unsigned int) const;
+		T&			operator[](unsigned int) ;
+
+	private:
+		T	*data;
+		unsigned int len;
+};
+
+
+#include <cstddef>
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+
+template<typename T>
+Array<T>::Array():
+	data(NULL),
+	len(0)
+{
+}
+
+template<typename T>
+Array<T>::Array(unsigned int n):
+	data(new T[n]()),
+	len(n)
+{
+}
+
+template<typename T>
+Array<T>::Array(const Array<T>& other):
+	data(NULL),
+	len(0)
+{
+	*this = other;
+}
+
+template<typename T>
+Array<T>::~Array()
+{
+	len = 0;
+	delete[] data;
+}
+
+template<typename T>
+Array<T>&	Array<T>::operator=(const Array<T>& other)
+{
+	if (this->data != NULL)
+		delete[] this->data;
+	data = new T[other.len];
+	for (unsigned int i = 0; i < other.len; i++)
+	{
+		data[i] = other.data[i];
+	}
+	this->len = other.len;
+	return (*this);
+}
+
+void	throw_error_index(unsigned int i);
+
+template <typename T>
+const T&	Array<T>::operator[](unsigned int i) const
+{
+	if (i >= this->len)
+	{
+		throw_error_index(i);
+	}
+	return (this->data[i]);
+}
+
+template <typename T>
+T&		Array<T>::operator[](unsigned int i)
+{
+	if (i >= this->len)
+	{
+		throw_error_index(i);
+	}
+	return (this->data[i]);
+}
+
+void	throw_error_index(unsigned int i)
+{
+	std::string	str;
+	std::stringstream ss;
+	ss << "index ";
+	ss << i;
+	ss << " is out of range";
+	std::getline(ss, str);
+	throw std::out_of_range(str);
+}
+
+template<typename T>
+unsigned int	Array<T>::size()
+{
+	return (this->len);
+}
+
+template<typename T>
+std::ostream&	operator<<(std::ostream& os, Array<T> arr)
+{
+	os << "there are " << arr.size() << " elements: ";
+	for (unsigned int i = 0; i < arr.size(); i++)
+		os << "'" << arr[i] << "', ";
+	os << "\n";
+	return (os);
+}
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jweber <jweber@student.42Lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 13:53:22 by jweber            #+#    #+#             */
+/*   Updated: 2026/03/04 13:53:30 by jweber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <stdexcept>
+#include "Array.cpp"
+
+
+int	main(void)
+{
+	Array<std::string> arr(3);
+	Array<std::string> arr2;
+
+	try
+	{
+		arr[1] = "coucou";
+		arr[2] = "toi";
+
+		std::cout << "arr[1] = "  << arr[1] << "\n";
+
+		arr2 = arr;
+		std::cout << "arr = " << arr << "\n";
+		std::cout << "arr2 = " << arr2 << "\n";
+
+		arr2[2] = "eheh";
+		std::cout << "arr = " << arr << "\n";
+		std::cout << "arr2 = " << arr2 << "\n";
+
+		arr[3] = 4;
+	}
+	catch (std::out_of_range& e)
+	{
+		std::cout << e.what() << "\n";
+	}
+}
