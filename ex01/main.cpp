78949ed62b5ab3b249d428a550d69616f942ad55
jweber <jweber@student.42Lyon.fr>
2026-06-22 14:48:54 +0200

    add new test in the main file

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -14,6 +14,38 @@
 #include "iter.hpp"
 
 template <typename T>
+void increment(T& a);
+
+template <typename T>
+void print(const T& a);
+
+void print_str(const std::string& a);
+
+int	main(void)
+{
+	//foo a[] = {foo(3), foo(4), foo(1), foo(-1)};
+	{
+		std::string a[] = {"coucou", "toi" ,"chou"};
+		std::cout << "sizeof(a)/sizeof(foo) = " << sizeof(a)/sizeof(*a) << "\n";
+		iter(a, sizeof(a)/sizeof(*a), print);
+		std::cout << "\n";
+		iter(a, sizeof(a)/sizeof(*a), print_str);
+		std::cout << "\n\n\n";
+	}
+
+	{
+		int a[] = {1,2,3};
+		std::cout << "sizeof(a)/sizeof(foo) = " << sizeof(a)/sizeof(*a) << "\n";
+		iter(a, sizeof(a)/sizeof(*a), print);
+		std::cout << "\n";
+		iter(a, sizeof(a)/sizeof(*a), increment);
+		std::cout << "\n";
+		iter(a, sizeof(a)/sizeof(*a), print);
+		std::cout << "\n\n\n";
+	}
+}
+
+template <typename T>
 void print(const T& a)
 {
 	std::cout << a << ", ";
@@ -25,43 +57,7 @@ void increment(T& a)
 	a++;
 }
 
-void increment_int(int& a)
-{
-	a++;
-}
-
 void print_str(const std::string& a)
 {
 	std::cout << "str = " << a << ", ";
-}
-
-/*
-class foo
-{
-public:
-	foo():a(0){};
-	foo(int val):a(val){};
-	~foo(){};
-	foo&	operator+=(int);	
-	int	a;
-};
-
-std::ostream& operator<<(std::ostream& os, const foo& ref)
-{
-	std::cout << ref.a;
-	return (os);
-}
-
-foo&	operator+=(int);	
-*/
-
-int	main(void)
-{
-	//foo a[] = {foo(3), foo(4), foo(1), foo(-1)};
-	std::string a[] = {"coucou", "toi" ,"chou"};
-	std::cout << "sizeof(a)/sizeof(foo) = " << sizeof(a)/sizeof(*a) << "\n";
-	iter(a, sizeof(a)/sizeof(*a), print);
-	std::cout << "\n";
-	iter(a, sizeof(a)/sizeof(*a), print_str);
-	std::cout << "\n";
 }
